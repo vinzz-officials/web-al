dc40d3a087fb9f045d8775c5c58a762f70dd3df3
vinzz-officials <vinzz4358@gmail.com>
2025-09-14 16:24:51 +0800

    Update blocked-ips.js

--- public/blocked-ips.js
+++ public/blocked-ips.js
@@ -1,14 +1,17 @@
 // blocked-ips.js
 let blockedIPs = [];
 
+// Ambil daftar IP yang diblokir
 export function getBlocked() {
   return blockedIPs;
 }
 
+// Tambah IP ke blocked
 export function addBlocked(ip) {
   if (!blockedIPs.includes(ip)) blockedIPs.push(ip);
 }
 
+// Hapus IP dari blocked
 export function removeBlocked(ip) {
   blockedIPs = blockedIPs.filter(i => i !== ip);
 }
