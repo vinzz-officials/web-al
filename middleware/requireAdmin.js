295c722201fd63220a29b62b44f1952c658ba22c
vinzz-officials <vinzz4358@gmail.com>
2025-09-14 15:12:53 +0800

    Delete middleware directory

--- middleware/requireAdmin.js
+++
@@ -1,9 +0,0 @@
-export function requireAdmin(req, res) {
-  const token = req.cookies?.adminToken;
-  if (token !== "Control Web by Vinzz") {
-    res.writeHead(302, { Location: "/login.html" });
-    res.end();
-    return null;
-  }
-  return { username: "Vinzz" };
-}
