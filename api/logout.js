d78c12674134ea53e1681530cf38a117e70cb1ce
vinzz-officials <vinzz4358@gmail.com>
2025-09-14 15:27:25 +0800

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,7 +1,7 @@
 export default function handler(req, res) {
   res.setHeader(
     "Set-Cookie",
-    "admin_token=; HttpOnly; Path=/; Max-Age=0"
+    "adminToken=; HttpOnly; Path=/; Max-Age=0"
   );
   res.status(200).json({ success: true });
 }
