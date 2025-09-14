4a5f9465d7fd424efba2d1214471c9d954fddf90
vinzz-officials <vinzz4358@gmail.com>
2025-09-14 15:36:05 +0800

    Update logout.js

--- api/logout.js
+++ api/logout.js
@@ -1,7 +1,16 @@
+import cookie from "cookie";
+
 export default function handler(req, res) {
+  // Hapus cookie adminToken
   res.setHeader(
     "Set-Cookie",
-    "adminToken=; HttpOnly; Path=/; Max-Age=0"
+    cookie.serialize("adminToken", "", {
+      httpOnly: true, // samain sama saat login (biar server yang kelola)
+      secure: process.env.NODE_ENV === "production",
+      maxAge: 0, // hapus cookie
+      path: "/",
+    })
   );
+
   res.status(200).json({ success: true });
 }
